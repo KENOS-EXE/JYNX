0d089cb834c66e8f9426cb67ef753cc9b3ea2b4f
FLASH-EXE <166912770+FLASH-EXE@users.noreply.github.com>
2024-05-23 19:52:22 +0545

    Update jynx.py

--- jynx.py
+++ jynx.py
@@ -1,26 +1,9 @@
-import platform
+import jynx
 import os
+import request
 
-global arch
+print('checking for update')
+time.sleep()
+os.system('git pull')
 
-def check_python_architecture():
-    global arch
-    architecture = platform.architecture()
-    if architecture[0] == '32bit':
-        arch = "32BIT"
-    elif architecture[0] == '64bit':
-        arch = "64BIT"
-    else:
-        arch = "INVALID"
-
-def main():
-    global arch
-    print("\033[1;37 •\033[1;36 ×\033[1;37 CHECKING FOR UPDATES")
-    os.system("git pull --quiet")
-    print(f"\033[1;37 •\033[1;36 ×\033[1;37 {arch} DETECED")
-    print("\033[1;37 •\033[1;36×\033[1;37 STARTING JYNX")
-    os.system("python jynx.so")
-
-if __name__ == "__main__":
-    check_python_architecture()
-    main()
+jynx.submit(menu)
