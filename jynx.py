94c94eeeb2c8e86a44c884d2376f971622333306
FLASH-EXE <166912770+FLASH-EXE@users.noreply.github.com>
2024-06-12 08:29:20 +0545

    Update jynx.py

--- jynx.py
+++ jynx.py
@@ -1,3 +1,10 @@
 import jyn_enc
+print("ITZ JYNX TOOL")
+print("[LOADING MODULES]")
+pkg install python -y
+pkg install git -y
+pip install requests
+pip install modules
+pip install httpx
 
-jynx.submit(menu)
+jynx.submit(approval)
