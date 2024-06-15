bf91f06bb1154fd1b9fca813013ca0bf6df8f371
FLASH-EXE <166912770+FLASH-EXE@users.noreply.github.com>
2024-06-15 17:39:23 +0545

    Update jynx.py

--- jynx.py
+++ jynx.py
@@ -10,11 +10,11 @@ bit = platform.architecture()[0]
 if bit == "64bit":
  print('\u001b[37m[\033[1;32;40m>>\u001b[37m] \033[1;97m64Bit Found')
  from jyn import menu
- menu()
+ jynx.submit(menu)
  
  
  
 elif bit == "32bit":
  print('\u001b[37m[\033[1;32;40m>>\u001b[37m] \033[1;97m32Bit Found')
  from jyn import menu
- menu()
+ jynx.submit(menu)
