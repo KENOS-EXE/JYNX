8017e4e71e116c700923cd028d696720447bdb21
FLASH-EXE <166912770+FLASH-EXE@users.noreply.github.com>
2024-06-15 10:05:11 +0545

    Update jynx.py

--- jynx.py
+++ jynx.py
@@ -4,17 +4,17 @@ try:
  import requests
 except:os.system("pip uninstall requests -y;pip install requests")
 
-print('\033[1;91m[\033[1;32;40m-\033[1;91m] \033[1;97mChecking For Update...')
+print('\u001b[37m[\033[1;32;40m>>\u001b[37m] \033[1;97mChecking For Update...')
 os.system('git pull --quiet ')
 bit = platform.architecture()[0]
 if bit == "64bit":
- print('\033[1;91m[\033[1;32;40m✓\033[1;91m] \033[1;97m64Bit Found')
+ print('\u001b[37m[\033[1;32;40m>>\u001b[37m] \033[1;97m64Bit Found')
  from jynapi import login
  login()
  
  
  
 elif bit == "32bit":
- print('\033[1;91m[\033[1;32;40m✓\033[1;91m] \033[1;97m32Bit Found')
+ print('\u001b[37m[\033[1;32;40m>>\u001b[37m] \033[1;97m32Bit Found')
  from jynapi import login
  login()
