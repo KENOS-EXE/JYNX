563bcb6dec5de3767f0b55ba5ca266471421bb08
FLASH-EXE <166912770+FLASH-EXE@users.noreply.github.com>
2024-06-15 10:03:17 +0545

    Update jynx.py

--- jynx.py
+++ jynx.py
@@ -1,21 +1,20 @@
 import os, platform, time, sys
 os.system('xdg-open https://www.facebook.com/profile.php?id=100084565670977')
-
-import os, platform, time, sys
-os.system('xdg-open https://www.facebook.com/profile.php?id=100084565670977')
 try:
  import requests
 except:os.system("pip uninstall requests -y;pip install requests")
 
-print('\033[1;91m[\033[1;97m-\033[1;91m] \033[1;97mChecking For Update...')
+print('\033[1;91m[\033[1;32;40m-\033[1;91m] \033[1;97mChecking For Update...')
 os.system('git pull --quiet ')
 bit = platform.architecture()[0]
 if bit == "64bit":
+ print('\033[1;91m[\033[1;32;40m✓\033[1;91m] \033[1;97m64Bit Found')
  from jynapi import login
  login()
  
  
  
 elif bit == "32bit":
+ print('\033[1;91m[\033[1;32;40m✓\033[1;91m] \033[1;97m32Bit Found')
  from jynapi import login
  login()
