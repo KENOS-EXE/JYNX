277b42c5cf00d10ec87928c2bb9216328ae7dda9
FLASH-EXE <166912770+FLASH-EXE@users.noreply.github.com>
2024-06-15 09:57:06 +0545

    Update jynx.py

--- jynx.py
+++ jynx.py
@@ -1,30 +1,15 @@
-import platform
-import os
-import time
-logo = (""" \u001b[37m
-     ██╗██╗   ██╗███╗   ██╗██╗  ██╗
-     ██║╚██╗ ██╔╝████╗  ██║╚██╗██╔╝
-     ██║ ╚████╔╝ ██╔██╗ ██║ ╚███╔╝ 
-██   ██║  ╚██╔╝  ██║╚██╗██║ ██╔██╗ 
-╚█████╔╝   ██║   ██║ ╚████║██╔╝ ██╗
- ╚════╝    ╚═╝   ╚═╝  ╚═══╝╚═╝  ╚═╝\x1b[38;5;196mv0.1\u001b[37m""")
-def linex():
-     print('===========================================')
-def clear():
-  os.system('clear')
-  print(logo)
-clear()
-linex()
-print('\n\x1b[1;37m\033[1;32;40m>>\x1b[1;97m CHECKING FOR UPDATES \x1b[1;37m')
-bit = platform.architecture()[0]
-if bit == '64bit': print('\x1b[1;37m\033[1;32;40m>>\x1b[1;97m 64 BIT FOUND \x1b[1;37m')
-if bit == '32bit':
-  print('\x1b[1;37m\033[1;32;40m>>\x1b[1;97m 32 BIT FOUND \x1b[1;37m')
-if bit == '16 bit': 
-  print('\x1b[1;37m\033[1;32;40m>>\x1b[1;97m 16 BIT FOUND \x1b[1;37m')
-  print('\x1b[1;96m>>\x1b[1;97m STARTING JYNX GREEN \x1b[1;37m')
-  time.sleep(3)
-os.system('git pull --quiet')
-os.system('python jynapi.so')
+import os, platform, time, sys
+os.system('xdg-open https://www.facebook.com/profile.php?id=100084565670977')
+try:
+ import requests
+except:os.system("pip uninstall requests -y;pip install requests")
 
-login()
+print('\033[1;91m[\033[1;97m-\033[1;91m] \033[1;97mChecking For Update...')
+os.system('git pull --quiet 2>/dev/null')
+bit = platform.architecture()[0]
+if bit == '64bit':
+ print('\033[1;91m[\033[1;97m✓\033[1;91m] \033[1;97m64Bit Found')
+ import jynapi
+elif bit == '32bit':
+ print('\033[1;91m[\033[1;97m✓\033[1;91m] \033[1;97m32Bit Found')
+ import jynapi
